48e8412ad332109b10cf8f8e8fc84d58990edd64
Adil-Gadi <58380369+Adil-Gadi@users.noreply.github.com>
2026-01-28 21:04:59 -0800

    Initial commit

---
+++ src/main/java/frc/robot/Constants.java
@@ -0,0 +1,80 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot;
+
+import static edu.wpi.first.units.Units.Degrees;
+import static edu.wpi.first.units.Units.Feet;
+import static edu.wpi.first.units.Units.Inches;
+
+import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.first.math.geometry.Rotation3d;
+import edu.wpi.first.math.geometry.Transform2d;
+import edu.wpi.first.math.geometry.Transform3d;
+import edu.wpi.first.math.geometry.Translation3d;
+import edu.wpi.first.units.measure.Distance;
+
+/**
+ * The Constants class provides a convenient place for teams to hold robot-wide numerical or boolean
+ * constants. This class should not be used for any other purpose. All constants should be declared
+ * globally (i.e. public static). Do not put anything functional in this class.
+ *
+ * <p>It is advised to statically import this class (or one of its inner classes) wherever the
+ * constants are needed, to reduce verbosity.
+ */
+public final class Constants {
+  public static class OperatorConstants {
+    public static final int kDriverControllerPort = 0;
+  }
+
+  public static class Vision {
+    public static final String LEFT_CAMERA = "LeftCamera";
+    public static final String RIGHT_CAMERA = "RightCamera";
+
+    // Robot Centric Transforms
+    public static final Transform3d ROBOT_TO_LEFT_CAMERA = new Transform3d(
+      new Translation3d(
+        Inches.of(6.5),
+        Inches.of(13.352),
+        Inches.of(15.378)
+      ),
+      new Rotation3d(
+        Degrees.of(0),
+        Degrees.of(30),
+        Degrees.of(90)
+      )
+    );
+
+    public static final Transform3d ROBOT_TO_RIGHT_CAMERA = new Transform3d(
+      new Translation3d(
+        Inches.of(6.5),
+        Inches.of(-13.352),
+        Inches.of(15.378)
+      ),
+      new Rotation3d(
+        Degrees.of(0),
+        Degrees.of(30),
+        Degrees.of(90)
+      )
+    );
+
+    public static final Transform3d LEFT_CAMERA_TO_ROBOT = ROBOT_TO_LEFT_CAMERA.inverse();
+    public static final Transform3d RIGHT_CAMERA_TO_ROBOT = ROBOT_TO_RIGHT_CAMERA.inverse();
+
+    public static final int BLUE_ALLIANCE_HUB_TAG = 26;
+    public static final int RED_ALLIANCE_HUB_TAG = 10; // TODO: Update Tag ID
+
+    public static final Transform2d BLUE_ALLIANCE_TAG_TO_HUB = new Transform2d(
+        Feet.of(1),
+        Feet.of(0),
+        new Rotation2d(Degrees.of(0))
+    );
+
+    public static final Transform2d RED_ALLIANCE_TAG_TO_HUB = new Transform2d(
+        Feet.of(-1),
+        Feet.of(0),
+        new Rotation2d(Degrees.of(0))
+    );
+  }
+}
---
+++ src/main/java/frc/robot/commands/PointToHub.java
@@ -0,0 +1,193 @@
+package frc.robot.commands;
+
+import static edu.wpi.first.units.Units.MetersPerSecond;
+import static edu.wpi.first.units.Units.RadiansPerSecond;
+import static edu.wpi.first.units.Units.RotationsPerSecond;
+
+import java.util.Optional;
+
+import org.photonvision.PhotonCamera;
+
+import com.ctre.phoenix6.swerve.SwerveModule.DriveRequestType;
+import com.ctre.phoenix6.swerve.SwerveRequest;
+
+import edu.wpi.first.math.controller.ProfiledPIDController;
+import edu.wpi.first.math.geometry.Rotation3d;
+import edu.wpi.first.math.geometry.Transform3d;
+import edu.wpi.first.math.geometry.Translation3d;
+import edu.wpi.first.math.trajectory.TrapezoidProfile;
+import edu.wpi.first.wpilibj.DriverStation;
+import edu.wpi.first.wpilibj.DriverStation.Alliance;
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
+import frc.robot.Constants;
+import frc.robot.generated.TunerConstants;
+import frc.robot.subsystems.CommandSwerveDrivetrain;
+
+public class PointToHub extends Command {
+
+    public static double MaxSpeed = TunerConstants.kSpeedAt12Volts.in(MetersPerSecond); // kSpeedAt12Volts desired top
+                                                                                        // speed
+    private static double MaxAngularRate = RotationsPerSecond.of(0.75).in(RadiansPerSecond); // 3/4 of a rotation per
+                                                                                             // second max angular
+                                                                                             // velocity
+
+    PhotonCamera leftCamera = new PhotonCamera(Constants.Vision.LEFT_CAMERA);
+    PhotonCamera rightCamera = new PhotonCamera(Constants.Vision.RIGHT_CAMERA);
+
+    ProfiledPIDController yawController;
+
+    CommandSwerveDrivetrain drivetrain;
+    CommandXboxController controller;
+
+    SwerveRequest.FieldCentric drive = new SwerveRequest.FieldCentric()
+            .withDeadband(MaxSpeed * 0.025).withRotationalDeadband(MaxAngularRate * 0.025) // Add a 10% deadband
+            .withDriveRequestType(DriveRequestType.OpenLoopVoltage); // Use open-loop control for drive motors
+
+    Transform3d tagToHub;
+
+    // Align to this tag
+    public int tag;
+
+    public double yawOutput;
+
+    public PointToHub(CommandSwerveDrivetrain drivetrain, CommandXboxController controller) {
+
+        this.controller = controller;
+
+        Optional<Alliance> alliance = DriverStation.getAlliance();
+
+        if (alliance.isEmpty()) {
+            end(false);
+            return;
+        }
+
+        if (alliance.get() == Alliance.Blue) {
+            tag = Constants.Vision.BLUE_ALLIANCE_HUB_TAG;
+            var t = Constants.Vision.BLUE_ALLIANCE_TAG_TO_HUB;
+            this.tagToHub = new Transform3d(
+                    new Translation3d(
+                            t.getX(),
+                            t.getY(),
+                            0.0),
+                    new Rotation3d(
+                            0.0,
+                            0.0,
+                            t.getRotation().getRadians()));
+        } else {
+            tag = Constants.Vision.RED_ALLIANCE_HUB_TAG;
+            var t = Constants.Vision.RED_ALLIANCE_TAG_TO_HUB;
+            this.tagToHub = new Transform3d(
+                    new Translation3d(
+                            t.getX(),
+                            t.getY(),
+                            0.0),
+                    new Rotation3d(
+                            0.0,
+                            0.0,
+                            t.getRotation().getRadians()));
+        }
+
+        this.drivetrain = drivetrain;
+
+        TrapezoidProfile.Constraints yawConstraints = new TrapezoidProfile.Constraints(1.33 * 0.75, 1.5 * 0.75);
+        yawController = new ProfiledPIDController(7.0, 0, 0, yawConstraints);
+
+        yawController.setTolerance(0.05 * 3);
+
+        this.addRequirements(drivetrain);
+    }
+
+    @Override
+    public void initialize() {
+        var driveState = drivetrain.getState();
+        var drivePose = driveState.Pose;
+        var driveSpeeds = driveState.Speeds;
+
+        yawController.reset(drivePose.getRotation().getRadians(), driveSpeeds.omegaRadiansPerSecond);
+    }
+
+    @Override
+    public void execute() {
+        var leftResults = leftCamera.getAllUnreadResults();
+        var rightResults = rightCamera.getAllUnreadResults();
+
+        if (leftResults.isEmpty() && rightResults.isEmpty()) {
+            return;
+        }
+
+        Transform3d robotToHub = null;
+
+        if (!leftResults.isEmpty()) {
+            // Camera processed a new frame since last
+            // Get the last one in the list.
+            var result = leftResults.get(leftResults.size() - 1);
+
+            if (result.hasTargets()) {
+                // At least one AprilTag was seen by the camera
+                for (var target : result.getTargets()) {
+
+                    if (target.getFiducialId() != tag) {
+                        continue;
+                    }
+
+                    Transform3d cameraToTag = target.getBestCameraToTarget();
+                    Transform3d robotToCamera = Constants.Vision.ROBOT_TO_LEFT_CAMERA;
+
+                    robotToHub = robotToCamera.plus(cameraToTag).plus(tagToHub);
+                    break;
+                }
+            }
+        }
+
+        if (robotToHub == null && rightResults.isEmpty() == false) {
+            var result = rightResults.get(rightResults.size() - 1);
+            if (result.hasTargets()) {
+                // At least one AprilTag was seen by the camera
+                for (var target : result.getTargets()) {
+                    if (target.getFiducialId() == tag) {
+                        Transform3d cameraToTag = target.getBestCameraToTarget();
+                        Transform3d robotToCamera = Constants.Vision.ROBOT_TO_RIGHT_CAMERA;
+
+                        robotToHub = robotToCamera.plus(cameraToTag).plus(tagToHub);
+                        break;
+                    }
+                }
+            }
+        }
+
+
+        if (robotToHub == null) {
+            control(this.yawOutput);
+            return;
+        }
+
+        double x = robotToHub.getTranslation().getX();
+        double y = robotToHub.getTranslation().getY();
+        double targetYaw = Math.atan2(y, x);
+
+        double currentYaw = drivetrain.getState().Pose.getRotation().getRadians();
+
+        yawController.setGoal(targetYaw);
+        this.yawOutput = yawController.calculate(currentYaw);
+
+        control(this.yawOutput);
+    }
+
+    public void control(double yawOutput) {
+
+        drive.withRotationalRate(yawOutput)
+                .withVelocityX(Math.pow(controller.getLeftY(), 3) * MaxSpeed)
+                .withVelocityY(Math.pow(controller.getLeftX(), 3) * MaxSpeed);
+
+        drivetrain.setControl(
+                drive);
+    }
+
+    @Override
+    public void end(boolean interrupted) {
+        drivetrain.setControl(new SwerveRequest.FieldCentric().withRotationalRate(0));
+
+        super.end(interrupted);
+    }
+}
